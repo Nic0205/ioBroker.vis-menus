7abfceba26708385502a890ac9f017acbd13ca42
Nic0205 <nick@communicate-online.de>
2015-12-08 20:44:27 +0100

    Commit message

---
+++ widgets/vis-menus/js/vis-menus.js
@@ -0,0 +1,68 @@
+/*
+    ioBroker.vis vis-menus Widget-Set
+
+    version: "0.0.1"
+
+    Copyright 10.2015-2016 Nick0205<nick@communicate-online.de>
+
+*/
+"use strict";
+
+// add translations for edit mode
+if (vis.editMode) {
+    $.extend(true, systemDictionary, {
+        "myColor":          {"en": "myColor",       "de": "mainColor",  "ru": "Мой цвет"},
+        "myColor_tooltip":  {
+            "en": "Description of\x0AmyColor",
+            "de": "Beschreibung von\x0AmyColor",
+            "ru": "Описание\x0AmyColor"
+        },
+        "htmlText":         {"en": "htmlText",      "de": "htmlText",   "ru": "htmlText"},
+        "group_extraMyset": {"en": "extraMyset",    "de": "extraMyset", "ru": "extraMyset"},
+        "extraAttr":        {"en": "extraAttr",     "de": "extraAttr",  "ru": "extraAttr"}
+    });
+}
+
+// add translations for non-edit mode
+$.extend(true, systemDictionary, {
+    "Instance":  {"en": "Instance", "de": "Instanz", "ru": "Инстанция"}
+});
+
+// this code can be placed directly in vis-menus.html
+vis.binds.vis-menus = {
+    version: "0.0.1",
+    showVersion: function () {
+        if (vis.binds.vis-menus.version) {
+            console.log('Version vis-menus: ' + vis.binds.vis-menus.version);
+            vis.binds.vis-menus.version = null;
+        }
+    },
+	createWidget: function (widgetID, view, data, style) {
+        var $div = $('#' + widgetID);
+        // if nothing found => wait
+        if (!$div.length) {
+            return setTimeout(function () {
+                vis.binds.vis-menus.createWidget(widgetID, view, data, style);
+            }, 100);
+        }
+
+        var text = '';
+        text += 'OID: ' + data.oid + '</div><br>';
+        text += 'OID value: <span class="myset-value">' + vis.states[data.oid + '.val'] + '</span><br>';
+        text += 'Color: <span style="color: ' + data.myColor + '">' + data.myColor + '</span><br>';
+        text += 'extraAttr: ' + data.extraAttr + '<br>';
+        text += 'Browser instance: ' + vis.instance + '<br>';
+        text += 'htmlText: <textarea readonly style="width:100%">' + (data.htmlText || '') + '</textarea><br>';
+
+        $('#' + widgetID).html(text);
+
+        // subscribe on updates of value
+        if (vis.states[data.oid]) {
+            vis.states.bind(data.oid + '.val', function (e, newVal, oldVal) {
+                $div.find('.vis-menus-value').html(newVal);
+            });
+        }
+    }
+};
+	
+vis.binds.vis-menus.showVersion();
